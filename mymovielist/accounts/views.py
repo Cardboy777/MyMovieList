291a073e58900339e05d4e08c34e629625d5bc14
macked@purdue.edu <dcmacke@gmail.com>
2018-04-09 23:24:06 -0400

    Updated Accounts Homepage

--- mymovielist/accounts/views.py
+++ mymovielist/accounts/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('Accounts Home Page!')
+	return render(request, 'accounts/home.html')
