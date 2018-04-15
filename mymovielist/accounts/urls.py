488a84b689fcc32b35bf4caf5a272c86811da1f6
macked@purdue.edu <dcmacke@gmail.com>
2018-04-15 15:12:08 -0400

    Added Login, Logout, Register, and Basic Profile

--- mymovielist/accounts/urls.py
+++ mymovielist/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
-	path('',views.home),
+	path('',views.home, name = 'home'),
 	path('login/',login,{'template_name':'accounts/login.html'}),
-	path('profile/',views.home),
+	path('logout/',logout,{'template_name':'accounts/logout.html'}),
+	path('register/',views.register, name='register'),
+	path('profile/',views.profile, name='profile'),
 ]
--- mymovielist/accounts/views.py
+++ mymovielist/accounts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-	number = {1,2,3,4,5}
-	name ='Daniel'
-	args = {'myname':name,'numbers':number}
-	return render(request, 'accounts/home.html', args)
+	return render(request, 'accounts/home.html')
+def register(request):
+	if request.method=='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/account/profile')
+	else:
+		form = UserCreationForm()
+
+		args = {'form':form}
+		return render(request, 'accounts/register.html',args)
+def profile(request):
+	args = {'User':request.user}
+	return render(request,'accounts/profile.html',args)
