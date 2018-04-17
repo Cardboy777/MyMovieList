14e7b46e9a26436ab8c423e6d922070f056d81dd
carr63 <carr63@purdue.edu>
2018-04-17 12:36:37 -0400

    Revert "Revert "Merge branch 'master' of https://github.com/Cardboy777/cs252project6""
    This reverts commit 9c758c3da600f7d364fcc7b36a020c6af71e145c.

--- mymovielist/accounts/views.py
+++ mymovielist/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import PasswordChangeForm, UserCreationForm
-from accounts.forms import RegistrationForm, EditProfileForm
+from accounts.forms import RegistrationForm, EditProfileForm, EditUserForm
 from accounts.models import UserProfile
 from django.contrib.auth.models import User
 from django.contrib.auth import update_session_auth_hash
@@ -32,6 +32,22 @@ def view_profile(request):
 
 
 def edit_profile(request):
+<<<<<<< HEAD
+	if request.method=='POST':
+		UserForm = EditUserForm(request.POST, instance=request.user)
+		ProfileForm = EditProfileForm(request.POST, instance=request.user.userprofile)
+		if UserForm.is_valid() and ProfileForm.is_valid():
+			UserForm.save()
+			ProfileForm.save()
+			return redirect('/account/profile')
+		else:
+			return redirect('/account/profile/edit')
+	else:
+		UserForm = EditUserForm(instance=request.user)
+		ProfileForm = EditProfileForm(instance=request.user.userprofile)
+		args = {'userform':UserForm,'profileform':ProfileForm}
+		return render(request,'accounts/edit_profile.html',args)
+=======
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
         if form.is_valid():
@@ -45,6 +61,7 @@ def edit_profile(request):
         return render(request, 'accounts/edit_profile.html', args)
 
 
+>>>>>>> 94a88021ae16388ad939060baa75f0eddeb04a00
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
