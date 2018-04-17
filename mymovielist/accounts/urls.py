992261f9794f36ff466fdef339418e4e5252e28a
carr63 <carr63@purdue.edu>
2018-04-17 12:34:09 -0400

    /mymovielist links to correct app

--- mymovielist/accounts/urls.py
+++ mymovielist/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import login, logout
-
+from django.conf.urls import include, url
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', login, {'template_name': 'accounts/login.html'}),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/password/', views.change_password, name='change_password'),
+    url(r'^mymovielist/', include('mymovielist.urls')),
 ]
