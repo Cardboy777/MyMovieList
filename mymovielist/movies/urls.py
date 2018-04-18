a59da45498dea622a259d38ed53ecd8192a74c2f
macked@purdue.edu <dcmacke@gmail.com>
2018-04-18 12:01:52 -0400

    Working Search Bar

--- mymovielist/movies/urls.py
+++ mymovielist/movies/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.movies)
+    path('', views.movies),
+    path('search/', views.search, name='search_url'),
 ]
--- mymovielist/movies/views.py
+++ mymovielist/movies/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 
 def movies(request) :
     return render(request, 'movies/movies.html')
+def search(request) :
+    search_query = request.GET.get('search_box')
+    if search_query:
+        args = {'search':search_query}
+        return render(request,'movies/search.html',args)
+    else:
+        return redirect('/')
