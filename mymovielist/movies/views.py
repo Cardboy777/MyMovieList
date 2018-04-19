21a2f4349fab614e3869ae74ed651175601dbe87
carr63 <carr63@purdue.edu>
2018-04-19 16:31:31 -0400

    added tmdbsimple
    make sure to do pip install tmdbsimple

--- mymovielist/movies/views.py
+++ mymovielist/movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-
+import tmdbsimple as tmdb
+tmdb.API_KEY = 'dd1efeb24fd2185a41514dc64bb9ac02'
 def movies(request) :
+    movie_id = request.GET.get('movie_id')
+    movie = tmdb.Movies(movie_id)
+    movie_info = movie.info()
     return render(request, 'movies/movies.html')
 def search(request) :
     search_query = request.GET.get('search_box')
