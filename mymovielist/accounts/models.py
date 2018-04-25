688a06b90c2fc70b3d758f49b77932841213c01e
DMack98 <dcmacke@gmail.com>
2018-04-25 23:37:42 +0000

    Age Safety validators

--- mymovielist/accounts/models.py
+++ mymovielist/accounts/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Django Secure User")
     description = models.CharField(max_length=1000, default='Enter Something about your Movie Tastes and Background')
     city = models.CharField(max_length=30, default='ZA WARUDO')
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(default=0
+	    ,validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ])
 
     # reviews = models.ManyToManyField(MovieReview, verbose_name="List of Reviewed Movies")
 
